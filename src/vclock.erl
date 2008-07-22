3da0714ecb00da0c3a99a2a14ef127966c19e6ca
shino_shun <shino_shun@0bfd8f84-bd4d-0410-8d16-8d8d8937d33b>
2008-07-22 12:38:04 +0000

    Format vclock.erl. Add vclock module to Makefile at src dir.

--- src/vclock.erl
+++ src/vclock.erl
@@ -1,6 +1,6 @@
 %% @copyright 2007-2008 Basho Technologies
 
-%% @reference Leslie Lamport (1978). "Time, clocks, and the ordering of events in a distributed system". Communications of the ACM 21 (7): 558-565. 
+%% @reference Leslie Lamport (1978). "Time, clocks, and the ordering of events in a distributed system". Communications of the ACM 21 (7): 558-565.
 
 %% @reference Friedemann Mattern (1988). "Virtual Time and Global States of Distributed Systems". Workshop on Parallel and Distributed Algorithms: pp. 215-226
 
@@ -29,30 +29,30 @@
 -author('Andy Gross <andy@basho.com>').
 
 -export([fresh/0,descends/2,merge/1,get_counter/2,get_timestamp/2,
-	increment/2,all_nodes/1]).
+         increment/2,all_nodes/1]).
 -export([example_test/0]).
 
-% @type vclock() = [vc_entry].
-% @type   vc_entry() = {node(), {counter(), timestamp()}}.
-% The timestamp is present but not used, in case a client wishes to inspect it.
-% @type   node() = term().
-% Nodes can have any term() as a name, but they must differ from each other.
-% @type   counter() = integer().
-% @type   timestamp() = integer().
+%% @type vclock() = [vc_entry].
+%% @type   vc_entry() = {node(), {counter(), timestamp()}}.
+%% The timestamp is present but not used, in case a client wishes to inspect it.
+%% @type   node() = term().
+%% Nodes can have any term() as a name, but they must differ from each other.
+%% @type   counter() = integer().
+%% @type   timestamp() = integer().
 
-% @doc Create a brand new vclock.
-% @spec fresh() -> vclock()
+%% @doc Create a brand new vclock.
+%% @spec fresh() -> vclock()
 fresh() ->
     [].
 
-% @doc Reflect an update performed by Node.
-%      See increment/2 for usage.
-% @spec extend(VC_Entry :: VC_Entry, VClock :: vclock()) -> vclock()
+%% @doc Reflect an update performed by Node.
+%%      See increment/2 for usage.
+%% @spec extend(VC_Entry :: VC_Entry, VClock :: vclock()) -> vclock()
 extend({Node,{Ctr,TS}}, VClock) ->
     simplify([{Node,{Ctr,TS}}|VClock]).
 
-% @doc Remove trivial ancestors.
-% @spec simplify(vclock()) -> vclock()
+%% @doc Remove trivial ancestors.
+%% @spec simplify(vclock()) -> vclock()
 simplify(VClock) ->
     simplify(VClock, []).
 simplify([], NClock) ->
@@ -60,16 +60,16 @@ simplify([], NClock) ->
 simplify([{Node,{Ctr1,TS1}}|VClock], NClock) ->
     {Ctr2,TS2} = proplists:get_value(Node, NClock, {Ctr1,TS1}),
     {Ctr,TS} = if
-	      Ctr1 > Ctr2 ->
-		  {Ctr1,TS1};
-	      true ->
-		  {Ctr2,TS2}
-	  end,
+                   Ctr1 > Ctr2 ->
+                       {Ctr1,TS1};
+                   true ->
+                       {Ctr2,TS2}
+               end,
     simplify(VClock, [{Node,{Ctr,TS}}|proplists:delete(Node, NClock)]).
 
 %% @todo Use common_test or other good test framework, and write more tests.
-%
-% @doc Serves as both a trivial test and some example code.
+%%
+%% @doc Serves as both a trivial test and some example code.
 example_test() ->
     A = vclock:fresh(),
     B = vclock:fresh(),
@@ -87,33 +87,33 @@ example_test() ->
     false = vclock:descends(B1, A1),
     ok.
 
-% @doc Return true if Va is a direct descendant of Vb, else false -- remember, a vclock is its own descendant!
-% @spec descends(Va :: vclock(), Vb :: vclock()) -> bool()
+%% @doc Return true if Va is a direct descendant of Vb, else false -- remember, a vclock is its own descendant!
+%% @spec descends(Va :: vclock(), Vb :: vclock()) -> bool()
 descends(_, []) ->
-    % all vclocks descend from the empty vclock
+    %% all vclocks descend from the empty vclock
     true;
 descends(Va, Vb) ->
     [{NodeB, {CtrB, _T}}|RestB] = Vb,
-    CtrA = 
-	case proplists:get_value(NodeB, Va) of
-	    undefined ->
-		false;
-	    {CA, _TSA} -> CA
-	end,
+    CtrA =
+        case proplists:get_value(NodeB, Va) of
+            undefined ->
+                false;
+            {CA, _TSA} -> CA
+        end,
     case CtrA of
-	false -> false;
-	_ -> 
-	    if
-		CtrA < CtrB ->
-		    false;
-		true ->
-		    descends(Va,RestB)
-	    end
+        false -> false;
+        _ ->
+            if
+                CtrA < CtrB ->
+                    false;
+                true ->
+                    descends(Va,RestB)
+            end
     end.
 
-% @doc Combine all VClocks in the input list into their least possible
-%      common descendant.
-% @spec merge(VClocks :: [vclock()]) -> vclock()
+%% @doc Combine all VClocks in the input list into their least possible
+%%      common descendant.
+%% @spec merge(VClocks :: [vclock()]) -> vclock()
 merge(VClocks) ->
     merge(VClocks, []).
 merge([], NClock) ->
@@ -121,35 +121,35 @@ merge([], NClock) ->
 merge([AClock|VClocks],NClock) ->
     merge(VClocks, lists:foldl(fun(X,L) -> extend(X,L) end, NClock, AClock)).
 
-% @doc Get the counter value in VClock set from Node.
-% @spec get_counter(Node :: node(), VClock :: vclock()) -> counter()
+%% @doc Get the counter value in VClock set from Node.
+%% @spec get_counter(Node :: node(), VClock :: vclock()) -> counter()
 get_counter(Node, VClock) ->
     case proplists:get_value(Node, VClock) of
-	{Ctr, _TS} -> Ctr;
-	undefined -> undefined
+        {Ctr, _TS} -> Ctr;
+        undefined -> undefined
     end.
 
-% @doc Get the timestamp value in a VClock set from Node.
-% @spec get_timestamp(Node :: node(), VClock :: vclock()) -> timestamp()
+%% @doc Get the timestamp value in a VClock set from Node.
+%% @spec get_timestamp(Node :: node(), VClock :: vclock()) -> timestamp()
 get_timestamp(Node, VClock) ->
     case proplists:get_value(Node, VClock) of
-	{_Ctr, TS} -> TS;
-	undefined -> undefined
+        {_Ctr, TS} -> TS;
+        undefined -> undefined
     end.
 
-% @doc Increment VClock at Node.
-% @spec increment(Node :: node(), VClock :: vclock()) -> vclock()
+%% @doc Increment VClock at Node.
+%% @spec increment(Node :: node(), VClock :: vclock()) -> vclock()
 increment(Node, VClock) ->
     {Ctr, TS} = case proplists:get_value(Node, VClock) of
-		    undefined ->
-			{1, timestamp()};
-		    {C, _T} ->
-			{C + 1, timestamp()}
-		end,
+                    undefined ->
+                        {1, timestamp()};
+                    {C, _T} ->
+                        {C + 1, timestamp()}
+                end,
     extend({Node, {Ctr, TS}}, VClock).
 
-% @doc Return the list of all nodes that have ever incremented VClock.
-% @spec all_nodes(VClock :: vclock()) -> [node()]
+%% @doc Return the list of all nodes that have ever incremented VClock.
+%% @spec all_nodes(VClock :: vclock()) -> [node()]
 all_nodes(VClock) ->
     [X || {X,{_,_}} <- VClock].
 
